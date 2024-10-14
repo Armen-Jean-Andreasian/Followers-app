f1f0e4148815920ff408885756fe20c035f3cac3
Armen-Jean Andreasian <armen_andreasian@proton.me>
2024-10-14 21:05:32 +0700

    Removed .env.yaml. Added .env.development, .env.production, .env.test
    Removed docker-compose.yml. Added docker-compose.development.yml,docker-compose.production.yml,docker-compose.test.yml

--- config/application.rb
+++ config/application.rb
@@ -2,43 +2,25 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
-
-# using this file migration was successful
-# however, with load_env initializer it wasn't working
-# and throwing KeyError: key not found: "DB_USERNAME" (KeyError)
-# Did you mean?  "USERNAME"
-# Uncomment the line 60 of this file, and run `rails db:create db:migrate` to reproduce the error
-def temp_loader
-  require 'yaml'
-
-  env_file = Rails.root.join('.env.yaml')
+def load_env_variables
+  env_file = Rails.root.join(".env.#{Rails.env}")
 
   if File.exist?(env_file)
-    current_env = Rails.env
-
     begin
-      env_variables = YAML.load_file(env_file)[current_env]
-
-      unless env_variables
-        raise StandardError.new("Environment configuration for #{current_env} not found in #{env_file}.")
-      end
-
-      env_variables.each do |key, value|
-        ENV[key] = value.to_s if value.present?
+      File.readlines(env_file).each do |line|
+        next if line.strip.empty? || line.start_with?('#')
+        key, value = line.split('=', 2).map(&:strip)
+        ENV[key] = value if value.present?
       end
     rescue StandardError => e
       raise StandardError.new("Error loading environment variables: #{e.message}")
     end
   else
-    raise StandardError.new("#{env_file} not found. Ask the origin to provide it.")
+    raise StandardError.new("#{env_file} not found. Please provide it.")
   end
 end
-
 
 module SocialApp
   class Application < Rails::Application
@@ -57,6 +39,6 @@ module SocialApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    temp_loader
+    load_env_variables
   end
 end
